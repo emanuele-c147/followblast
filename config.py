cd231940e9e2100896c46ea796892e67c9e534d6
emanuele-c147 <emanuelecanazza08@gmail.com>
2026-09-13 22:57:41 +0200

    refactor(main): extract parsers into a dedicated file

--- config.py
+++ config.py
@@ -2,7 +2,8 @@
 Configuration file for DB and FILES
 """
 
-from main import parse_follower, parse_following, export_to_html, export_to_csv, export_to_json, export_to_txt
+from main import export_to_html, export_to_csv, export_to_json, export_to_txt
+from parsers import parse_follower, parse_following
 
 FILES_CONFIG = {
     "followers": {
